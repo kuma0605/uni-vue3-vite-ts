fb08d0fa6f14ca275bdc8c3b9b8b33157295ccc6
dylan <825880445@qq.com>
2023-05-18 19:09:52 +0800

    添加sass

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,10 @@
 <template>
   <view class="content">
-    <custom-nav  :hide-back="true">
+    <custom-nav :hide-back="true">
       自定义导航栏
     </custom-nav>
-    
-    <image class="logo" src="/static/logo.png" @tap="goHome"/>
+
+    <image class="logo" src="/static/logo.png" @tap="goHome" />
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
@@ -15,39 +15,40 @@
 import { ref } from 'vue'
 const title = ref('Hello')
 
-function goHome(){
+function goHome() {
   uni.switchTab(
     {
-      url:"/pages/home/home"
+      url: "/pages/home/home"
     }
   )
 }
 </script>
-
-<style>
+<style lang="scss">
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+  .text-area {
+    display: flex;
+    justify-content: center;
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+    .title {
+      font-size: 36rpx;
+      color: #8f8f94;
+    }
+  }
+
+
 }
 </style>
