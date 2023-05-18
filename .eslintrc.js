4b8428f899926a962645a5f3936ccd7dd652ed2f
dylan <825880445@qq.com>
2023-05-19 00:26:13 +0800

    添加配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'vue/multi-word-component-names':"off",
-    },
-    "globals": {
-        "uni": true, 
-        "wx": true
-      }
-}
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module"
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "vue/multi-word-component-names": "off"
+  },
+  globals: {
+    uni: true,
+    wx: true
+  }
+};
