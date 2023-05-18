66d56f2077cda5727cc1dcaa1ee0bb91329c4c22
dylan <825880445@qq.com>
2023-05-18 21:47:29 +0800

    添加eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'vue/multi-word-component-names':"off",
+    },
+    "globals": {
+        "uni": true, 
+        "wx": true
+      }
+}
